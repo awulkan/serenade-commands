b9bcf2c5adea1a6c9b275cfc8af52782d87d1db4
Alexander <alexanderwulkan@hotmail.com>
2021-09-18 02:19:40 +0200

    Fix: Code formatting

--- input/keys.js
+++ input/keys.js
@@ -1,5 +1,5 @@
-serenade.global().key("pop","backspace");
-serenade.global().key("home","home");
-serenade.global().key("end","end");
+serenade.global().key("pop", "backspace");
+serenade.global().key("home", "home");
+serenade.global().key("end", "end");
 serenade.global().key("go top", "home", ["control"]);
 serenade.global().key("go bottom", "end", ["control"]);
--- input/symbols.js
+++ input/symbols.js
@@ -1,30 +1,30 @@
 const CHAIN = require('../constants/chaining.js');
 
-serenade.global().key("bang","!", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("question","?", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("period",".", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("point",".", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("dot",".", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("comma",",", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("colon",":", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("semi",";", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("greater than",">", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("less than","<", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("equals","=", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("slash","/", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("backslash","\\", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("hash","#", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("dollar","$", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("underscore","_", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("apos","'", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("apostrophe","'", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("quote","\"", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("paren","(", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("right paren",")", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("brace","{", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("right brace","}", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("bracket","[", undefined, CHAIN.AUTO_ANY);
-serenade.global().key("right bracket","]", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("bang", "!", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("question", "?", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("period", ".", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("point", ".", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("dot", ".", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("comma", ",", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("colon", ":", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("semi", ";", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("greater than", ">", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("less than", "<", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("equals", "=", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("slash", "/", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("backslash", "\\", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("hash", "#", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("dollar", "$", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("underscore", "_", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("apos", "'", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("apostrophe", "'", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("quote", "\"", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("paren", "(", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("right paren", ")", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("brace", "{", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("right brace", "}", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("bracket", "[", undefined, CHAIN.AUTO_ANY);
+serenade.global().key("right bracket", "]", undefined, CHAIN.AUTO_ANY);
 serenade.global().command(
   "spam",
   async (api, matches) => {
