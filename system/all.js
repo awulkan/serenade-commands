10fb43515a4829ec89fdb65b2b51a75da3984397
Alexander <alexanderwulkan@hotmail.com>
2021-10-03 00:44:28 +0200

    Add: Debug commands

--- system/all.js
+++ system/all.js
@@ -12,3 +12,13 @@ serenade.global().command("zoom in", async (api, matches) => {
 serenade.global().command("zoom out", async (api, matches) => {
   await api.pressKey("-", [KEY.COMMAND_OR_CONTROL]);
 });
+
+serenade.global().command("get running applications", async (api, matches) => {
+  const apps = await api.getRunningApplications();
+  await api.typeText(apps.toString());
+});
+
+serenade.global().command("get active application", async (api, matches) => {
+  const app = await api.getActiveApplication();
+  await api.typeText(app);
+});
